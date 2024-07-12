f50b960111c2ff34f8bdad0a61af7a74abb522b3
Sarath Chandran <sarathchandran1771@gmail.com>
2024-07-12 13:08:59 +0530

    Add initial root files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import NotFound from './pages/Error/NotFound';
+import CreateProject from './pages/CreateProject/CreateProject';
+import ProjectList from './pages/Projects/ProjectList';
+import { Navigate } from 'react-router-dom';
+
+
+const PrivateRoute = ({ element }) => {
+  const token = localStorage.getItem('data_token');
+  return token ? element : <Navigate to="/" replace />;
+}
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Login/>,
+    errorElement: <NotFound/>
+  },
+  {
+    path: '/dashboard',
+    element: <PrivateRoute element={<Home />} />,
+  },
+  {
+    path: '/project-List',
+    element: <PrivateRoute element={<ProjectList />} />,
+  },
+  {
+    path: '/create-project',
+    element: <PrivateRoute element={<CreateProject />} />,
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
